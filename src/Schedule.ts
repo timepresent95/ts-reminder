7a4b370688ddeb461c1f1eb0aebccd510c5b28a5
timepresent95 <timepresent@gmail.com>
2023-05-15 21:17:08 +0900

    Feat: Schedule 클래스 구현

---
+++ src/Schedule.ts
@@ -0,0 +1,13 @@
+export class Schedule {
+    title: string;
+    notes: string;
+    isCompleted: boolean;
+    key: string;
+
+    constructor(title: string, notes: string, key: string) {
+        this.title = title;
+        this.notes = notes;
+        this.key = key;
+        this.isCompleted = false;
+    }
+}
