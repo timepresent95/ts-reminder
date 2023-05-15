ca8f82eb5c4cf182c7b81b4e18ed0154c60df8c0
timepresent95 <timepresent@gmail.com>
2023-05-15 21:16:51 +0900

    Feat: TypeGuard 구현

---
+++ src/elementTypeGuard.ts
@@ -0,0 +1,3 @@
+export const isListElement = (el: Element | undefined): el is HTMLLIElement => el && el.tagName === 'LI';
+export const isInputElement = (el: Element | undefined): el is HTMLLIElement => el && el.tagName === 'INPUT';
+export const isButtonElement = (el: Element | undefined): el is HTMLLIElement => el && el.tagName === 'BUTTON';
