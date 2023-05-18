359c79a6572ec5fd80da16c5d540bb978d2b4027
timepresent95 <timepresent@gmail.com>
2023-05-18 16:53:23 +0900

    chore: eslint, prettier 설정 및 tsconfig 수정

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,9 @@
-import * as webpack from "webpack";
+import webpack from "webpack";
 import 'webpack-dev-server';
 
-import * as path from "path";
-import * as HtmlWebpackPlugin from "html-webpack-plugin";
-import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const isProduction = process.env.NODE_ENV === "production";
 
